90ce71908b6bac2308bb8ab5aa28692352140803
Jonesdy99 <jonesdylan1165@gmail.com>
2022-07-25 16:18:27 -0400

    implement chart.js

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -1,9 +1,16 @@
 import styles from './Landing.module.css'
+import Graph from '../../components/Graph/Graph'
 
 const Landing = ({ user }) => {
   return (
     <main className={styles.container}>
       <h1>hello, {user ? user.name : 'friend'}</h1>
+      <div className='container mx-auto max-w-6xl text-center drop-shadow-lg'>
+      <h2 className='text-4xl py-10 mb-10 bg-slate-800 text-white'>Expense Tracker</h2>
+      </div>
+      <div>
+        <Graph></Graph>
+      </div>
     </main>
   )
 }
